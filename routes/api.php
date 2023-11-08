247d0923773d115e730ca3b4b78752e6a7318038
muhammadusmantalat <usman0725.ranglerz@gmail.com>
2023-11-08 17:38:37 +0500

    apis

--- routes/api.php
+++ routes/api.php
@@ -43,18 +43,26 @@ Route::post('users-imageupdate/{id}', 'Api\ProfileController@updateImage');
 Route::middleware(['auth:sanctum'])->group(function () {
     //edit-user-profile
     //user-documents
-    Route::get('document-index/{id}', 'Api\DocumentController@index');
-    Route::get('document-index/{id}', 'Api\DocumentController@index');
-    //user-vehicles
-    Route::get('driver-vehicle-index/{id}', 'Api\DriverVehicleController@index');
-    Route::get('driver-vehicle-create/{id}', 'Api\DriverVehicleController@create');
-    Route::post('driver-vehicle-add/{id}', 'Api\DriverVehicleController@store');
+    // Route::get('document-index/{id}', 'Api\DocumentController@index');
+    // Route::get('document-index/{id}', 'Api\DocumentController@index');
+    // //user-vehicles
+    // Route::get('driver-vehicle-index/{id}', 'Api\DriverVehicleController@index');
+    // Route::get('driver-vehicle-create/{id}', 'Api\DriverVehicleController@create');
+    // Route::post('driver-vehicle-add/{id}', 'Api\DriverVehicleController@store');
     //owner-jobs
     // Route::post('owners-jobs/{id}', 'Api\JobController@jobStore');
 
     //get owners jobs
     Route::get('owners-jobs-show/{id}', 'Api\OwnerJobsController@index');
 });
+
+Route::get('document-index/{id}', 'Api\DocumentController@index');
+Route::get('document-index/{id}', 'Api\DocumentController@index');
+//user-vehicles
+Route::get('driver-vehicle-index/{id}', 'Api\DriverVehicleController@index');
+Route::get('driver-vehicle-create/{id}', 'Api\DriverVehicleController@create');
+Route::post('driver-vehicle-add/{id}', 'Api\DriverVehicleController@store');
+//owner-jobs
 Route::post('owners-jobs/{id}', 'Api\JobController@jobStore');
 Route::post('owners-reviews', 'Api\ReviewController@ownerReview');
 Route::get('driver-reviews/{id}', 'Api\ReviewController@showFeedBackToDriver');
