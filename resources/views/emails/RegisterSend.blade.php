95f06bdd59dc8ac83b735eb155a0a79d87fce00e
muhammadusmantalat <usman0725.ranglerz@gmail.com>
2023-11-09 11:27:46 +0500

    emailschanges

--- resources/views/emails/RegisterSend.blade.php
+++ resources/views/emails/RegisterSend.blade.php
@@ -2,8 +2,7 @@
 
 # Congrulation you have signed up successfully.
 
-{{-- <p>This is your account password.</p>
-<h3>{{$password}}</h3> --}}
+ 
 
 @if ($status === 'Owner')
     You are successfully registered as {{$status}}.
