635f93f0057c73617b5e752799dd86ce66f8aa19
muhammadusmantalat <usman0725.ranglerz@gmail.com>
2023-11-14 19:03:39 +0500

    changes

--- app/Http/Controllers/Admin/WalletController.php
+++ app/Http/Controllers/Admin/WalletController.php
@@ -30,4 +30,28 @@ class WalletController extends Controller
         $withdraw_requests = WithdrawalRequest::with('bankAccount','user')->get();
         return view('admin.withdrawal_requests.index',compact('withdraw_requests'));
     }
+    public function send_money(Request $request , $id)
+    {
+        $withdraw = WithdrawalRequest::find($id);
+        $drivrr_id = $withdraw->driver_id;
+        $driver_wallet = DriverWallet::find($drivrr_id);
+        return $driver_wallet;
+        $request->validate([
+             'image'=>'required',
+        ]);
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $extension = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $extension;
+            $file->move(public_path('admin/assets/images/approve/'), $filename);
+            $image = 'public/admin/assets/images/approve/' . $filename;
+        }
+        else {
+            $image = 'public/admin/assets/images/approve/owner.jpg';
+        }
+        // $user = DriverWallet::
+        // $approve = DriverWallet::create([
+
+        // ]);
+    }
 }
