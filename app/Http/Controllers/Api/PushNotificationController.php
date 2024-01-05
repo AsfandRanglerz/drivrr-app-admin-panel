6148fb483b99b7dedc51976f8fcc73f6e1feaa81
BilalRanglerz12 <bilal.rangler199@gmail.com>
2024-01-05 14:54:33 +0500

    APIS

--- app/Http/Controllers/Api/PushNotificationController.php
+++ app/Http/Controllers/Api/PushNotificationController.php
@@ -11,35 +11,35 @@ class PushNotificationController extends Controller
     public function getNotification(Request $request, $userId)
     {
         try {
-            $notifications = PushNotification::where('user_id', $userId)->where('seen_by', 0)->get();
-            PushNotification::where('user_id', $userId)->where('seen_by', 0)->update(['seen_by' => 1]);
+            $notifications = PushNotification::where('user_id', $userId)->get();
+
             return response()->json([
                 'status' => 'success',
-                'message' => 'Notifications retrieved and updated successfully',
+                'message' => 'Notifications retrieved successfully',
                 'notifications' => $notifications,
             ], 200);
         } catch (\Exception $e) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Failed to retrieve and update notifications',
+                'message' => 'Failed to retrieve notifications',
                 'error' => $e->getMessage(),
             ], 500);
         }
     }
-    // public function userRecevied(Request $request, $userId)
-    // {
-    //     try {
-    //         PushNotification::where('user_id', $userId)->update(['seen_by' => 1]);
-    //         return response()->json([
-    //             'status' => 'success',
-    //             'message' => 'Notifications marked as seen',
-    //         ], 200);
-    //     } catch (\Exception $e) {
-    //         return response()->json([
-    //             'status' => 'error',
-    //             'message' => 'Failed to retrieve notifications',
-    //             'error' => $e->getMessage(),
-    //         ], 500);
-    //     }
-    // }
+    public function userRecevied(Request $request, $userId)
+    {
+        try {
+            PushNotification::where('user_id', $userId)->update(['seen_by' => 1]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Notifications marked as seen',
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to retrieve notifications',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
 }
