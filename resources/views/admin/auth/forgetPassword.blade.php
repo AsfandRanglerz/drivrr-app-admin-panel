7b803c5930bc4a95375c5fa20a798d5b3a7eed2a
BilalRanglerz12 <bilal.ranglerz199@gmail.com>
2024-06-05 12:32:48 +0500

    Chnages

--- resources/views/admin/auth/forgetPassword.blade.php
+++ resources/views/admin/auth/forgetPassword.blade.php
@@ -5,9 +5,9 @@
         <div class="container mt-5">
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                    <div class="card card-primary">
-                        <div class="card-header">
-                            <h4>Forgot Password</h4>
+                    <div class="card card-dark mt-5">
+                        <div class=" text-center">
+                            <img src="{{ asset('public/admin/assets/img/blacklogo.png') }}" class="w-75" style="margin-top:40px">
                         </div>
                         <div class="card-body">
                             <p class="text-muted">We will send a link to reset your password</p>
@@ -18,8 +18,8 @@
                                     <input id="email" type="email" class="form-control" name="email" tabindex="1" >
                                     @error('email') <span class="text-danger">{{$errors->first('email')}}</span>@enderror
                                 </div>
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                <div class="form-group mb-0">
+                                    <button type="submit" class="btn btn-dark btn-lg btn-block" tabindex="4">
                                         Forgot Password
                                     </button>
                                 </div>
--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -5,9 +5,9 @@
         <div class="container mt-5">
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                    <div class="card card-primary">
-                        <div class="card-header">
-                            <h4>Login</h4>
+                    <div class="card card-dark mt-5">
+                        <div class=" text-center">
+                            <img src="{{ asset('public/admin/assets/img/blacklogo.png') }}" class="w-75" style="margin-top:40px">
                         </div>
                         <div class="card-body">
                             <form method="POST" action="{{ url('login') }}" class="needs-validation" novalidate="">
@@ -33,7 +33,7 @@
                                         <input type="checkbox" class="custom-control-input" tabindex="3" id="remember-me"
                                             name="remember">
                                         <div class="d-block">
-                                            <label class="custom-control-label" for="remember-me">Remember Me</label>
+                                            {{-- <label class="custom-control-label" for="remember-me">Remember Me</label> --}}
                                             <div class="float-right">
                                                 <a href="{{ url('admin-forgot-password') }}" class="text-small">
                                                     Forgot Password?
@@ -42,47 +42,31 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                <div class="form-group mb-0">
+                                    <button type="submit" class="btn btn-dark btn-lg btn-block" tabindex="4">
                                         Login
                                     </button>
                                 </div>
                             </form>
-                            {{-- <div class="text-center mt-4 mb-3">
-                                <div class="text-job text-muted">Login With Social</div>
-                            </div>
-                            <div class="row sm-gutters">
-                                <div class="col-6">
-                                    <a class="btn btn-block btn-social btn-facebook">
-                                        <span class="fab fa-facebook"></span> Facebook
-                                    </a>
-                                </div>
-                                <div class="col-6">
-                                    <a class="btn btn-block btn-social btn-twitter">
-                                        <span class="fab fa-twitter"></span> Twitter
-                                    </a>
-                                </div>
-                            </div> --}}
+
                         </div>
                     </div>
-                    {{--                    <div class="mt-5 text-muted text-center"> --}}
-                    {{--                        Don't have an account? <a href="{{ route('admin.register') }}">Create One</a> --}}
-                    {{--                    </div> --}}
+
                 </div>
             </div>
         </div>
     </section>
 @endsection
 @section('script')
-@if (\Illuminate\Support\Facades\Session::has('message'))
-<script>
-    toastr.success('{{ \Illuminate\Support\Facades\Session::get('message') }}');
-</script>
-@endif
+    @if (\Illuminate\Support\Facades\Session::has('message'))
+        <script>
+            toastr.success('{{ \Illuminate\Support\Facades\Session::get('message') }}');
+        </script>
+    @endif
 
-@if (\Illuminate\Support\Facades\Session::has('error'))
-<script>
-    toastr.error('{{ \Illuminate\Support\Facades\Session::get('error') }}');
-</script>
-@endif
+    @if (\Illuminate\Support\Facades\Session::has('error'))
+        <script>
+            toastr.error('{{ \Illuminate\Support\Facades\Session::get('error') }}');
+        </script>
+    @endif
 @endsection
