78e9e7e9bf4c8fda05f77663a496af995a7d0f78
BilalRanglerz12 <bilal.ranglerz199@gmail.com>
2024-11-20 10:21:43 +0500

    changes

--- app/Http/Controllers/Api/DriverShowJobsController.php
+++ app/Http/Controllers/Api/DriverShowJobsController.php
@@ -27,14 +27,14 @@ class DriverShowJobsController extends Controller
                 ->join('vehicles', 'jobs.vehicle_id', '=', 'vehicles.id')
                 ->where('driver_vehicles.user_id', $userId)
                 ->where('driver_vehicles.is_active', '=', '1')
-                ->where('jobs.on_vehicle', '=', '0')
+                ->where('jobs.on_vehicle', '=', '1')
                 ->select('jobs.*', 'users.fname', 'users.lname', 'users.email', 'users.image', 'vehicles.name')
                 ->get();
         } else {
             $getJobData = Job::select('jobs.*', 'users.*', 'vehicles.*')
                 ->join('users', 'jobs.user_id', '=', 'users.id')
                 ->join('vehicles', 'jobs.vehicle_id', '=', 'vehicles.id')
-                ->where('jobs.on_vehicle', '=', '1')
+                ->where('jobs.on_vehicle', '=', '0')
                 ->select('jobs.*', 'users.fname', 'users.lname', 'users.email', 'users.image', 'vehicles.name')
                 ->get();
         }
